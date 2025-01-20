8afc13e52debf0d5fccdaf5bbc2cd6ba830fbfd6
Fedor Shadrin <shadowfieng@gmail.com>
2025-01-20 18:11:55 +0700

    feat: implement bad variant of Counter

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter>
-        <Counter.Label>My super flexible counter</Counter.Label>
-        <Counter.Decrease icon="-" />
-        <Counter.Increase icon="+" />
-        <Counter.Count />
-      </Counter>
+      <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      />
     </div>
   )
 }
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,55 +1,34 @@
-import { createContext, ReactNode, useContext, useState } from 'react'
+import { ReactNode, useState } from 'react'
 
-type CounterContextValue = {
-  count: number
-  increase: () => void
-  decrease: () => void
+type CounterProps = {
+  iconIncrease: ReactNode
+  iconDecrease: ReactNode
+  label: string
+  hideLabel?: boolean
+  hideIncrease?: boolean
+  hideDecrease?: boolean
 }
 
-const CounterContext = createContext<CounterContextValue | null>(null)
-
-export const useCounterContext = () => {
-  const context = useContext(CounterContext)
-  if (!context) {
-    throw new Error('useCounterContext must be used within a CounterProvider')
-  }
-  return context
-}
-
-function Counter({ children }: { children: ReactNode }) {
+function Counter({
+  iconIncrease,
+  iconDecrease,
+  label,
+  hideLabel = false,
+  hideIncrease = false,
+  hideDecrease = false
+}: CounterProps) {
   const [count, setCount] = useState(0)
   const increase = () => setCount((c) => c + 1)
   const decrease = () => setCount((c) => c - 1)
 
   return (
-    <CounterContext.Provider value={{ count, increase, decrease }}>
-      <span>{children}</span>
-    </CounterContext.Provider>
+    <div>
+      {!hideLabel && <span>{label}</span>}
+      {!hideDecrease && <button onClick={decrease}>{iconDecrease}</button>}
+      <span>{count}</span>
+      {!hideIncrease && <button onClick={increase}>{iconIncrease}</button>}
+    </div>
   )
 }
-
-// 3. Create child components to help implementing the common task
-function Count() {
-  const { count } = useCounterContext()
-  return <span>{count}</span>
-}
-
-function Label({ children }: { children: ReactNode }) {
-  return <span>{children}</span>
-}
-
-function Increase({ icon }: { icon: ReactNode }) {
-  const { increase } = useCounterContext()
-  return <button onClick={increase}>{icon}</button>
-}
-
-function Decrease({ icon }: { icon: ReactNode }) {
-  const { decrease } = useCounterContext()
-  return <button onClick={decrease}>{icon}</button>
-}
-Counter.Count = Count
-Counter.Label = Label
-Counter.Increase = Increase
-Counter.Decrease = Decrease
 
 export default Counter
